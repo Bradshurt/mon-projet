d13b904bdec88070adb1e6528c0afa5eabf134d0
Bradshurt <yannbradley17@gmail.com>
2025-04-17 11:42:46 +0200

    les modification

--- includes/config.php
+++ includes/config.php
@@ -1,18 +1,18 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "los_angeles";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "los_angeles";
 
-// Connexion avec MySQLi
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // Connexion avec MySQLi
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Vérification de la connexion
-if ($conn->connect_error) {
-    die("Connexion échouée : " . $conn->connect_error);
-}
-echo "Connexion réussie !";
+    // Vérification de la connexion
+    if ($conn->connect_error) {
+        die("Connexion échouée : " . $conn->connect_error);
+    }
+    echo "Connexion réussie !";
 
 
 ?>
